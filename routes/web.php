5153b7acba751f670a4d09484e8eb5f78e779cf4
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-05-09 02:43:02 +0800

    Wrapping up the Section 35 -> 165. Creating the Additional Authentication Structure

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 02:40 05-09-2020 Authentication Route.
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// 02:40 05-09-2020 Registration.
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// 2:40 05-09-2020 Password Reset.
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// 2:39 05-09-2020 Email Verification.
+$this->get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+$this->get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+$this->get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Route::get('/home', 'HomeController@index')->name('home');
